a20f6239526085e79ea3df794fc4d931e039f282
Frostymyst <ethanwilson@shaw.ca>
2025-10-24 15:32:40 -0700

    Adjusted tests to say test_ as seen by sawyer

--- Test/test_productClass.py
+++ Test/test_productClass.py
@@ -9,32 +9,32 @@ from fastapi import FastAPI
 
 
 # Test if products are created.
-def testProductCreation():
+def test_ProductCreation():
     product = Product(1, "Basket Ball", "Bouncing ball for playing", 25.00)
     assert product.productID == 1
     assert product.productName == "Basket Ball"
 
 # Test if discounts can be set.
-def testSetDiscount():
+def test_SetDiscount():
     product = Product(2, "Hockey Stick", "Hockey Stick for Adults", 95.00)
     product.setDiscount(10)
     assert product.discountPercent == 10
     assert product.discount == 95 * (10/100)
 
 # Test if the price can be changed.
-def testChangePrice():
+def test_ChangePrice():
     product = Product(3, "Mug", "Coffee and Tea Mug", 15.00)
     product.changePrice(25.00)
     assert product.price == 25.00
 
 # Test for invalid discount application. Discount greater than 100.
-def testInvalidDiscount():
+def test_InvalidDiscount():
     product = Product(4, "Glasses", "Rayband summer glasses", 150.00)
     with pytest.raises(ValueError):
         product.setDiscount(300) 
 
 # Test for invalid price changes. Negative price.
-def testInvalidPriceChange():
+def test_InvalidPriceChange():
     product = Product(6, "Chair", "Lawn chair blue", 45.00)
     with pytest.raises(ValueError):
         product.changePrice(-45.00)
