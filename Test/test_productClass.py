cb9eed682d5177d921a53159a89c38e47329aa5c
Frostymyst <ethanwilson@shaw.ca>
2025-10-25 12:57:17 -0700

    updated to follow styling guidelines feedback from sawyer

--- Test/test_productClass.py
+++ Test/test_productClass.py
@@ -1,6 +1,6 @@
 # Testing productClass.py
 # Created by: Ethan Wilson
-# Reviewed by: (Insert Name)
+# Reviewed by: Sawyer
 from backend.app.schemas.productClass import Product
 import pytest
 import os
@@ -9,32 +9,32 @@ from fastapi import FastAPI
 
 
 # Test if products are created.
-def test_ProductCreation():
+def test_product_creation():
     product = Product(1, "Basket Ball", "Bouncing ball for playing", 25.00)
-    assert product.productID == 1
-    assert product.productName == "Basket Ball"
+    assert product.product_id == 1
+    assert product.product_name == "Basket Ball"
 
 # Test if discounts can be set.
-def test_SetDiscount():
+def test_set_discount():
     product = Product(2, "Hockey Stick", "Hockey Stick for Adults", 95.00)
-    product.setDiscount(10)
-    assert product.discountPercent == 10
-    assert product.discount == 95 * (10/100)
+    product.set_discount(10)
+    assert product._discount_percent == 10
+    assert product._discount == product._price * (10/100)
 
 # Test if the price can be changed.
-def test_ChangePrice():
+def test_change_price():
     product = Product(3, "Mug", "Coffee and Tea Mug", 15.00)
-    product.changePrice(25.00)
-    assert product.price == 25.00
+    product.change_price(25.00)
+    assert product._price == 25.00
 
 # Test for invalid discount application. Discount greater than 100.
-def test_InvalidDiscount():
+def test_invalid_discount():
     product = Product(4, "Glasses", "Rayband summer glasses", 150.00)
     with pytest.raises(ValueError):
-        product.setDiscount(300) 
+        product.set_discount(300) 
 
 # Test for invalid price changes. Negative price.
-def test_InvalidPriceChange():
+def test_invalid_pric_change():
     product = Product(6, "Chair", "Lawn chair blue", 45.00)
     with pytest.raises(ValueError):
-        product.changePrice(-45.00)
+        product.change_price(-45.00)
