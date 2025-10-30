975475c8f9dd38e1fa4a628371e20f8122e95e30
Frostymyst <ethanwilson@shaw.ca>
2025-10-29 17:19:00 -0700

    Updated to pass Pytest
    Let's see if this works for the update. Added setter and getters.

--- backend/app/schemas/productClass.py
+++ backend/app/schemas/productClass.py
@@ -3,13 +3,25 @@
 # Reviewed by: Sawyer
 
 
+import decimal
+
+
 class Product:
 
-    def __init__(self, product_id, product_name, product_desc, price, discount_price=0.0,
-                  discount_percent=0.0, rating=0, rating_count=0, units_sold=0):
-        self.product_id = product_id
-        self.product_name = product_name
-        self.product_desc = product_desc
+
+    def __init__(self,
+                product_id: int,
+                product_name: str,
+                product_desc: str,
+                price: decimal,
+                discount_price: decimal = 0,
+                discount_percent:decimal = 0.0,
+                rating: decimal = 0.0,
+                rating_count: int = 0,
+                units_sold: int = 0):
+        self._product_id = product_id
+        self._product_name = product_name
+        self._product_desc = product_desc
         self._price = price
         self._discount_price = discount_price
         self._discount_percent = discount_percent
@@ -17,13 +29,15 @@ class Product:
         self._rating_count = rating_count
         self._units_sold = units_sold
 
+
     # Set the discount price of the product. Percent should be int and within 0-100.
     def set_discount(self, percent):
         if percent < 0 or percent > 100:
             raise ValueError("Discount percent must be within 0 and 100.")
         self._discount_percent = percent
-        self._discount = self._price * (percent / 100)
+        self._discount_price = self._price * (percent / 100)
         print("Discount has been set successfully.")
+
 
     # Set the price to a new price. Price should be float greater than 0.
     def change_price(self, price):
@@ -33,3 +47,86 @@ class Product:
         print("New price set successfully.")
 
 
+    #Setters/Getters for all attributes of the class.
+    @property
+    def product_id(self):
+        return self._product_id
+   
+    @product_id.setter
+    def product_id(self, id:int):
+        self._product_id = id
+
+
+    @property
+    def product_name(self):
+        return self._product_name
+   
+    @product_name.setter
+    def product_name(self, id:str):
+        self._product_name = id
+
+
+    @property
+    def product_desc(self):
+        return self._product_desc
+
+
+    @product_desc.setter
+    def product_desc(self, id:str):
+        self._product_desc = id
+
+
+    @property
+    def price(self):
+        return self._price
+
+
+    @price.setter
+    def price(self, id:decimal):
+        self._price = id            
+
+
+    @property
+    def discount_price(self):
+        return self._discount_price
+
+
+    @discount_price.setter
+    def discount_price(self, id:decimal):
+        self._discount_price = id
+
+
+    @property
+    def discount_percent(self):
+        return self._discount_percent
+   
+    @discount_percent.setter
+    def discount_percent(self, id:int):
+        self._discount_percent = id
+
+
+    @property
+    def rating(self):
+        return self._rating
+   
+    @rating.setter
+    def rating(self, id:decimal):
+        self._rating = id
+
+
+    @property
+    def rating_count(self):
+        return self._rating_count
+   
+    @rating_count.setter
+    def rating_count(self, id:int):
+        self._rating_count = id
+
+
+    @property
+    def units_sold(self):
+        return self._units_sold
+   
+    @units_sold.setter
+    def units_sold(self, id:int):
+        self._units_sold = id
