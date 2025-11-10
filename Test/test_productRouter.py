b911b7739a7d2ebbdf7a939ed6355032872aff1a
sawyercairns <mrsawyercairns@gmail.com>
2025-11-09 16:17:04 -0800

    Product interaction get
    -Implemented product interactor get product
    -modified get product API endpoint to use product interactor methods
    -improved testing of product API

--- Test/test_productRouter.py
+++ Test/test_productRouter.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from backend.app.main import app
+import json
 
 client = TestClient(app)
 
@@ -8,11 +9,18 @@ client = TestClient(app)
 # Update tests to ensure these categories and filters return as expected as these methods are implemented
 
 def test_get_products():
-    r = client.get("/products?categories=test&keywords=test2&maxPrice=100")
+    r = client.get("/products?category=test&keyword=test2&maxPrice=100")
     assert r.status_code == 200
+    
     r = client.get("/products?maxPrice=100")
     assert r.status_code == 200
-    r = client.get("/products?keywords=test2&maxPrice=100")
+    response = r.json()
+    for item in response:
+        assert item["_price"] <= 100
+    
+
+    r = client.get("/products?keyword=USB")
     assert r.status_code == 200
-    r = client.get("/products?categories=test&maxPrice=100")
-    assert r.status_code == 200
+    response = r.json()
+    for item in response:
+        assert "USB" in item["_product_name"]
