f42fd58a20a3b3a29ea0743b111df79a01a5cb60
Frostymyst <ethanwilson@shaw.ca>
2025-11-11 12:32:34 -0800

    userValidation.py
    adding validator

--- backend/app/services/userValidation.py
+++ backend/app/services/userValidation.py
@@ -1,4 +1,20 @@
+import json
 
-#TODO: implenment this
-def user_is_valid(user_name, password):
-    return True
+
+# Takes user email, password, and the users.json to check if they exist
+# Returns either true or false
+def validate_user(uEmail, uPassword, fPath='users.json'):
+    try:
+        with open(fPath, 'r', encoding='utf-8') as file:
+            users = json.load(file)
+       
+        for user in users:
+            if user.get("email") == uEmail and user.get("user_password") == uPassword:
+                return True
+
+
+        return False
+   
+    except FileNotFoundError:
+        print("JSON file not found error")
+        return False
