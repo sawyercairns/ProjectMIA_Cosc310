9c8b6698e04d50c1f6ce4c7537075670f75af0ca
sawyercairns <mrsawyercairns@gmail.com>
2025-11-11 13:25:32 -0800

    Product Creation API implemented

--- backend/app/routers/productRouter.py
+++ backend/app/routers/productRouter.py
@@ -1,11 +1,20 @@
 from typing import List
 from fastapi import APIRouter, status
-from backend.app.schemas import productClass
+from backend.app.schemas.productClass import Product
 import json
 from backend.app.services import productInteractor
+from backend.app.services.userValidation import user_is_admin
 
 router = APIRouter(prefix="/products", tags=["products"])
 
 @router.get("", response_model=None)
 def get_products(category: str = "", keyword:str = "", maxPrice: float = 1000000):
     return productInteractor.get_products_filtered(category, keyword, maxPrice)
+
+@router.post("", response_model=None, status_code=201)
+def create_product(username:str, password:str, product_name:str, description:str, price:float):
+    if user_is_admin(username, password):
+        productInteractor.create_product(Product(0, product_name, description, price))
+        return "PRODUCT CREATED"
+    else: return "CREATION FAILED"
+    
