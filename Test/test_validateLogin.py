f36fb0ee7def1a217b6ae01b3723b89001f21b81
Frostymyst <ethanwilson@shaw.ca>
2025-11-13 00:51:49 -0800

    Attempt to pass with pytest validation
    ran it on root and had it pass pytest I want to see if this loads successfully this time

---
+++ Test/test_validateLogin.py
@@ -0,0 +1,22 @@
+# Testing validateLogin.py
+# Created by: Ethan Wilson
+# Reviewed by:
+
+
+import pytest
+import json
+from backend.app.services.validateLogin import validate_user
+from fastapi import FastAPI
+
+
+users_path = "backend/app/data/users.json"
+
+
+# Try to login with an invalid user.
+def test_fake_user():
+    assert validate_user("validateLogin@gmail.com", "test", users_path) == False
+
+
+# Check existing user and try to login.
+def test_real_user():
+    assert validate_user("test@test.com", "test", users_path) == True
