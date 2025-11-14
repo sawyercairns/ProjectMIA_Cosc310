73d1230c7883cc25e503c8fa6b12ce2b23387da4
sawyercairns <mrsawyercairns@gmail.com>
2025-11-13 16:21:22 -0800

    add user/ remove user functions
    -Added add_user to userInteractor
    -Added remove_user to userinteractor
    -Fixed some issues with get_user file path not being relative

--- backend/app/routers/userRouter.py
+++ backend/app/routers/userRouter.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from backend.app.services.userInteractor import get_user
+from backend.app.services.userInteractor import get_user, remove_user
 
 router = APIRouter(prefix="/login", tags=["login"])
 
@@ -12,3 +12,12 @@ def user_validation(email:str, password:str):
         return "VALID USER"
     else:
         return "INVALID USERNAME OR PASSWORD"
+
+@router.delete("{user_id}")
+def delete_user(user_id: str, email:str, password: str):
+    user = get_user(email, password)
+    if user is not None and user.is_admin:
+        remove_user(user_id)
+        return "USER REMOVED"
+    else:
+        return "REMOVAL FAILED"
