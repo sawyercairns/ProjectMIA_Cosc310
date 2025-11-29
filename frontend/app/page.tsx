9bf7000bed2e611c3da16636bca3d0dcff4f1219
sawyercairns <mrsawyercairns@gmail.com>
2025-11-29 12:44:56 -0800

    Improved Frontend main page
    -Frontend now pulls products using backend api, and constructs its own html.
    -Frontend no longer separates product list into pages (feel free to do this if you really want to, but trust me, it's not worth it.)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,38 @@
-'use client'
 import Form from 'next/form'
-import { useSearchParams } from 'next/navigation';
 
-export default async function Home() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get('search');
-  const page = searchParams.get('page');
-  const htmlContent = await fetchBrowsingPage(search, page);
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+   const items = await fetchProducts((await searchParams).query);
   return (
+    <div>
+      <h1>ProjectMIA Online Shop</h1>
     
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      <a href="/">🏠 Home</a>
+      <Form action="/">
+        <input name="query"  placeholder="Enter product name..."/>
+        <button type="submit">Submit</button>
+      </Form>
+      <ul>
+        {items.map(item => (
+          <li key={item._product_id}>
+          <strong>{ item._product_name }</strong><br></br>
+          {item._product_desc}<br></br>
+          Price: ${ item._price }<br></br>
+          Rating: { item._rating } ({ item._rating_count } reviews)<br></br>
+          Units sold: { item._units_sold }
+            <hr></hr>
+          </li>
+        ))}
+      </ul>
+        
+    </div> 
   );
 }
+
 
 async function fetchBrowsingPage(search:any, page:any) {
   try {
@@ -19,6 +40,17 @@ async function fetchBrowsingPage(search:any, page:any) {
     if (page == null) page = '1'
     const response = await fetch('http://localhost:8000/?search=' + search +'&page=' + page);
     const data = await response.text();
+    return data
+  } catch (error) {
+    console.error("Error fetching item:", error);
+  }
+}
+
+async function fetchProducts(keyword:any){
+  try {
+    if(keyword == null) keyword = ""
+    const response = await fetch('http://localhost:8000/products?keyword=' + keyword);
+    const data = await response.json();
     return data
   } catch (error) {
     console.error("Error fetching item:", error);
